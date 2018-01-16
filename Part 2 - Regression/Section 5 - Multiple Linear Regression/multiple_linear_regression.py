4da926dd912356fbfdbce2680329b487b3578f1c
Kobe Ruado <insidiae423@gmail.com>
2018-01-16 23:36:05 +0800

    Optimized MLR model using Backward Elimination

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -28,3 +28,30 @@ regressor.fit(X_train, y_train)
 
 # Predict Test Set results
 y_pred = regressor.predict(X_test)
+
+# Optimize model using Backward Elimination
+import statsmodels.formula.api as sm
+# Append a column on 1s to X
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+
+# Generate a matrix of optimal features for X
+# Start with including all of X's features
+# X_opt = X[:, [0, 1, 2, 3, 4, 5, 6]]
+# Select a Significance Level for features to stay in
+# e.g.: SL = 0.05
+# If highest P-value > SL, remove the feature with that P-value
+# Rerun this code segment until P values < SL
+X_opt = X[:, [0, 4]]
+regressor_ols = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_ols.summary()
+# Code to rerun stops here
+
+# Fit the optimal Linear Regression model using X's significant features
+# Use index 3 because X_train doesn't have that column of 1s.
+X_train_opt = X_train[:, [3]]
+X_test_opt = X_test[:, [3]]
+regressor_opt = LinearRegression()
+regressor_opt.fit(X_train_opt, y_train)
+
+# Predict Test Set results
+y_pred_opt = regressor_opt.predict(X_test_opt)

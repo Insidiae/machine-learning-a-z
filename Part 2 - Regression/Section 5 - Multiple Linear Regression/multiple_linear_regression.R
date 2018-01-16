11811f0a2fe6181061b3f53020abe9a6e2a5e468
Kobe Ruado <insidiae423@gmail.com>
2018-01-17 00:30:37 +0800

    Finished Multiple Linear Regression Section

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
@@ -21,3 +21,27 @@ regressor <- lm(formula = Profit ~ .,
 
 # Predict Test Set Results
 y_pred <- predict(regressor, newdata = test_set)
+
+# Optimize MLR Model using Backward Elimination
+# Start with including all features of the dataset
+# regressor_opt <- lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State,
+#                 data = dataset)
+# Select Significance Level for features to stay in
+# e.g.: SL = 0.05
+
+# summary(regressor_opt)
+# compare P-values (Pr(>|t|)) to SL
+# if highest P-value > SL, remove the feature with that P-value
+# Re-fit the regressor
+
+# Re-run this code until all features with P-values > SL are removed
+# NOTE: summary(regressor_opt) makes things easier for us here.
+# Asterisks (*) are added when P-values are lower than 0.05.
+# (Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1)
+# regressor_opt <- lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend,
+#                     data = dataset)
+# regressor_opt <- lm(formula = Profit ~ R.D.Spend + Marketing.Spend,
+#                     data = dataset)
+regressor_opt <- lm(formula = Profit ~ R.D.Spend,
+                    data = dataset)
+summary(regressor_opt)
